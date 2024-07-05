2ad857f61cde5d8b8c5e73a300668b747f4f13cd
Mrinalkbanik01 <manukbanik30@gmail.com>
2024-07-05 17:24:52 +0530

    Initial Commit

---
+++ store_index.py
@@ -0,0 +1,9 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
